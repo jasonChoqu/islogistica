1749ca82f5a2a636f7bdb15d03fc1247c3bc5121
jasonChoqu <jasonchoquecanaviri@gmail.com>
2025-01-23 18:59:43 -0400

    Initial commit

---
+++ config/Config.php
@@ -0,0 +1,14 @@
+<?php
+define('BASE_URL', 'http://localhost/islogistica/');
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBNAME', 'test');
+define('CHARSET', 'charset=utf8');
+define('TITLE', 'Sistema Punto de Venta');
+define('MONEDA', 'Bs');
+define('HOST_SMTP', 'mail.webbo.li');
+define('USER_SMTP', 'info@webbo.li');
+define('CLAVE_SMTP', '2Bez!(h-yCR=');
+define('PUERTO_SMTP', 465);
+?>
